13b2bfe3e92c1b8fd81a562b981c472fe79257d9
ajaxkulkarni <ajinkyashiva@gmail.com>
2020-11-12 13:35:27 +0530

    fields added

--- edofox-service/src/main/java/com/rns/web/edo/service/domain/EdoQuestionDetails.java
+++ edofox-service/src/main/java/com/rns/web/edo/service/domain/EdoQuestionDetails.java
@@ -7,8 +7,51 @@ public class EdoQuestionDetails {
 	private String paragraph_number;//": "", "
 	private String line_no;//": "", "
 	private String status;//": "1", "
-	private String short_explanation;//": "ddddd", "topics": "1"
+	private String short_explanation;//": "ddddd", "
+	private String topics;//": "1"
+	private Integer self_created;
+	private Integer neet_no_of_ques;
+	private Integer jee_no_of_ques;
+	private Integer setter_id;
+	private Integer board_id;
+
 	
+	public String getTopics() {
+		return topics;
+	}
+	public void setTopics(String topics) {
+		this.topics = topics;
+	}
+	public Integer getSelf_created() {
+		return self_created;
+	}
+	public void setSelf_created(Integer self_created) {
+		this.self_created = self_created;
+	}
+	public Integer getNeet_no_of_ques() {
+		return neet_no_of_ques;
+	}
+	public void setNeet_no_of_ques(Integer neet_no_of_ques) {
+		this.neet_no_of_ques = neet_no_of_ques;
+	}
+	public Integer getJee_no_of_ques() {
+		return jee_no_of_ques;
+	}
+	public void setJee_no_of_ques(Integer jee_no_of_ques) {
+		this.jee_no_of_ques = jee_no_of_ques;
+	}
+	public Integer getSetter_id() {
+		return setter_id;
+	}
+	public void setSetter_id(Integer setter_id) {
+		this.setter_id = setter_id;
+	}
+	public Integer getBoard_id() {
+		return board_id;
+	}
+	public void setBoard_id(Integer board_id) {
+		this.board_id = board_id;
+	}
 	public String getName_of_publication() {
 		return name_of_publication;
 	}
