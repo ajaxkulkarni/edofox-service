1c7617331cf2bbb40c50e5422614a25d40dd55eb
ajaxkulkarni <ajinkyashiva@gmail.com>
2020-11-27 18:24:34 +0530

    new fields added

---
+++ edofox-service/src/main/java/com/rns/web/edo/service/domain/EdoQuestionCriteria.java
@@ -0,0 +1,23 @@
+package com.rns.web.edo.service.domain;
+
+public class EdoQuestionCriteria {
+
+	private Integer id;
+	private String criteria;
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getCriteria() {
+		return criteria;
+	}
+	public void setCriteria(String criteria) {
+		this.criteria = criteria;
+	}
+	
+	
+	
+}
